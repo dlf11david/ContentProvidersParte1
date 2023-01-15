cd88083f9f0f1e2161cbaa95ff4a894dbca695c7
davidlopez <dfl11@alu.ua.es>
2023-01-15 21:54:59 +0100

    Página update lista

--- app/src/main/java/es/ua/eps/sqlite/UpdateUser.kt
+++ app/src/main/java/es/ua/eps/sqlite/UpdateUser.kt
@@ -1,7 +1,9 @@
 package es.ua.eps.sqlite
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import es.ua.eps.sqlite.databinding.ActivityUpdateUserBinding
 
@@ -9,7 +11,6 @@ class UpdateUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityUpdateUserBinding.inflate(layoutInflater)
-        val sqlUsers = SQLiteCode(this)
         setContentView(binding.root)
         with(binding) {
             btnUpdate.setOnClickListener {
@@ -17,13 +18,12 @@ class UpdateUser : AppCompatActivity() {
                     etUpPass.text.isNotBlank() &&
                     etUpName.text.isNotBlank()) {
 
-                    sqlUsers.updateData(
-                        selectedUser,
-                        "${etUpLogin.text}",
-                        "${etUpPass.text}",
-                        "${etUpName.text}",
-                        "${etUpLogin.text}@mail.ua"
-                    )
+                    val cv = ContentValues()
+                    cv.put(ProviderCode.NOMBRE_USUARIO,"${etUpLogin.text}")
+                    cv.put(ProviderCode.PASSWORD,"${etUpPass.text}")
+                    cv.put(ProviderCode.NOMBRE_COMPLETO,"${etUpName.text}")
+                    cv.put(ProviderCode.EMAIL,"${etUpLogin.text}@mail.ua")
+                    contentResolver.update(ProviderCode.CONTENT_URI,cv,"${ProviderCode.NOMBRE_USUARIO} = ?", arrayOf(selectedUser))
 
                     etUpLogin.text.clear()
                     etUpPass.text.clear()
